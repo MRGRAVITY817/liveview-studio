7fc5a7160896e27ec2b27964e772af193a63f1a7
Hoon Wee <mrgravity817@gmail.com>
2023-03-22 00:49:38 +0900

    refactor: extract custom component

--- lib/live_view_studio_web/live/boats_live.ex
+++ lib/live_view_studio_web/live/boats_live.ex
@@ -2,6 +2,7 @@ defmodule LiveViewStudioWeb.BoatsLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Boats
+  import LiveViewStudioWeb.CustomComponents
 
   def mount(_params, _session, socket) do
     socket =
@@ -85,23 +86,5 @@ defmodule LiveViewStudioWeb.BoatsLive do
     boats = Boats.list_boats(filter)
 
     {:noreply, assign(socket, boats: boats, filter: filter)}
-  end
-
-  attr :expiration, :integer, default: 24
-  slot :legal
-  slot :inner_block, required: true
-
-  def promo(assigns) do
-    ~H"""
-    <div class="promo">
-      <div class="deal">
-        <%= render_slot(@inner_block) %>
-      </div>
-      <div class="expiration">Deal expires in <%= @expiration %></div>
-      <div class="legal">
-        <%= render_slot(@legal) %>
-      </div>
-    </div>
-    """
   end
 end
