31e4ae6d2ef16900321d53cd33d3944c21ef5cca
Hoon Wee <mrgravity817@gmail.com>
2023-03-31 21:42:09 +0900

    feat: check form input in realtime in volunteer

--- lib/live_view_studio_web/live/volunteers_live.ex
+++ lib/live_view_studio_web/live/volunteers_live.ex
@@ -22,7 +22,7 @@ defmodule LiveViewStudioWeb.VolunteersLive do
     ~H"""
     <h1>Volunteer Check-In</h1>
     <div id="volunteer-checkin">
-      <.form for={@form} phx-submit="save">
+      <.form for={@form} phx-submit="save" phx-change="validate">
         <.input field={@form[:name]} placeholder="Name" autocomplete="off" />
         <.input
           field={@form[:phone]}
@@ -71,5 +71,14 @@ defmodule LiveViewStudioWeb.VolunteersLive do
         socket = put_flash(socket, :error, "Failed to check in.")
         {:noreply, assign_form(socket, changeset)}
     end
+  end
+
+  def handle_event("validate", %{"volunteer" => volunteer_params}, socket) do
+    changeset =
+      %Volunteer{}
+      |> Volunteers.change_volunteer(volunteer_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, form: to_form(changeset))}
   end
 end
