1a4f7abdbd25bd93052f9ed7bdeaa4e382442397
Hoon Wee <mrgravity817@gmail.com>
2023-04-07 22:12:39 +0900

    feat: add empty server struct in form

--- lib/live_view_studio_web/live/server_form_component.ex
+++ lib/live_view_studio_web/live/server_form_component.ex
@@ -1,3 +1,12 @@
 defmodule LiveViewStudioWeb.ServerFormComponent do
+  alias LiveViewStudio.Servers
+  alias LiveViewStudio.Servers.Server
+
   use LiveViewStudioWeb, :live_component
+
+  def mount(socket) do
+    # Create empty `Server` struct
+    changeset = Servers.change_server(%Server{})
+    {:ok, assign(socket, :form, to_form(changeset))}
+  end
 end
