467a58bd9028c914b06436e0439033a0a1506391
Hoon Wee <mrgravity817@gmail.com>
2023-04-09 10:12:10 +0900

    feat: use `on_mount()` hook to redo authentication when top secret page is accessed via websocket connection

--- lib/live_view_studio_web/live/top_secret_live.ex
+++ lib/live_view_studio_web/live/top_secret_live.ex
@@ -1,6 +1,10 @@
 defmodule LiveViewStudioWeb.TopSecretLive do
   use LiveViewStudioWeb, :live_view
 
+  # Redirection is done via websocket, which means it doesn't go through router pipeline.
+  # We should do manual check using `on_mount()` hook when this page is reached from websocket.
+  on_mount {LiveViewStudioWeb.UserAuth, :ensure_authenticated}
+
   def mount(_params, _session, socket) do
     {:ok, socket}
   end
@@ -12,12 +16,18 @@ defmodule LiveViewStudioWeb.TopSecretLive do
       <div class="mission">
         <h1>Top Secret</h1>
         <h2>Your Mission</h2>
-        <h3>(spy number here)</h3>
+        <h3><%= pad_user_id(@current_user) %></h3>
         <p>
           Storm the castle and capture 3 bottles of Elixir.
         </p>
       </div>
     </div>
     """
+  end
+
+  def pad_user_id(user) do
+    user.id
+    |> Integer.to_string()
+    |> String.pad_leading(3, "0")
   end
 end
