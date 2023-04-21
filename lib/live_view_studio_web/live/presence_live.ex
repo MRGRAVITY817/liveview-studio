0332603da8b28ca95d4abacecdc1047bb5aec7c2
Hoon Wee <mrgravity817@gmail.com>
2023-04-21 23:01:39 +0900

    feat: get presence by current user process

--- lib/live_view_studio_web/live/presence_live.ex
+++ lib/live_view_studio_web/live/presence_live.ex
@@ -1,16 +1,36 @@
 defmodule LiveViewStudioWeb.PresenceLive do
+  alias LiveViewStudioWeb.Presence
   use LiveViewStudioWeb, :live_view
 
+  @topic "users:video"
+
   def mount(_params, _session, socket) do
+    %{current_user: current_user} = socket.assigns
+
+    if connected?(socket) do
+      # track current process(= user)
+      {:ok, _} =
+        Presence.track(self(), @topic, current_user.id, %{
+          username: current_user.email |> String.split("@") |> hd(),
+          is_playing: false
+        })
+    end
+
+    presences = Presence.list(@topic)
+
     socket =
       socket
       |> assign(:is_playing, false)
+      |> assign(:presences, presences)
 
     {:ok, socket}
   end
 
   def render(assigns) do
     ~H"""
+    <pre>
+    <%= inspect(@presences, pretty: true) %>
+    </pre>
     <div id="presence">
       <div class="users">
         <h2>Who's Here?</h2>
