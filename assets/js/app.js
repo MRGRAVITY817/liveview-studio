80b5a8e8e73e0b2024e8ddbeb0468545f3d4e2db
Hoon Wee <mrgravity817@gmail.com>
2023-05-31 23:15:47 +0900

    feat: add Calendar JS hook

--- assets/js/app.js
+++ assets/js/app.js
@@ -22,17 +22,23 @@ import { Socket } from "phoenix"
 import { LiveSocket } from "phoenix_live_view"
 import topbar from "../vendor/topbar"
 
-let csrfToken =
-  document.querySelector("meta[name='csrf-token']").getAttribute("content")
+let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 
 let liveSocket = new LiveSocket("/live", Socket, {
-  params: { _csrf_token: csrfToken }
-});
+  params: { _csrf_token: csrfToken },
+  hooks: {
+    Calendar: {
+      mounted() {
+        console.log("Hook mounted!", this.el)
+      },
+    },
+  },
+})
 
 // Show progress bar on live navigation and form submits
 topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" })
-window.addEventListener("phx:page-loading-start", info => topbar.delayedShow(200))
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
+window.addEventListener("phx:page-loading-start", (info) => topbar.delayedShow(200))
+window.addEventListener("phx:page-loading-stop", (info) => topbar.hide())
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
